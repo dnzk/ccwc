3bef1b3884bae814727db2de214d9fadef3841ad
Danish Kalesaran <danishkalesaran@gmail.com>
2023-12-27 20:51:10 +0800

    feat: support all options

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,82 @@ use std::io::ErrorKind;
 use std::io::{stderr, stdout, Write};
 use std::process::ExitCode;
 
+enum CcOptions {
+    Bytes,
+    Words,
+    Lines,
+    Characters,
+}
+
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         return output("Insufficient arguments", 1);
     }
+    let options = parse_options(&args[1]);
     let file_path = &args[2];
     match File::open(&file_path) {
         Ok(mut file) => {
             let mut contents = String::new();
             file.read_to_string(&mut contents).unwrap();
-            return output(
-                format!("{0} has {1} bytes", &file_path, String::len(&contents)).as_str(),
-                0,
-            );
+            let mut report: Vec<String> = vec![];
+            for o in options.iter() {
+                match o {
+                    CcOptions::Lines => {
+                        let result = format!("{} lines", count_lines(&contents));
+                        report.push(result);
+                    }
+                    CcOptions::Characters => {
+                        let result = format!("{} characters", count_characters(&contents));
+                        report.push(result);
+                    }
+                    CcOptions::Words => {
+                        let result = format!("{} words", count_words(&contents));
+                        report.push(result);
+                    }
+                    CcOptions::Bytes => {
+                        let result = format!("{} bytes", count_bytes(&contents));
+                        report.push(result);
+                    }
+                }
+            }
+            let report = report.join(" ");
+            return output(&report, 0);
         }
         Err(error) => handle_file_error(error.kind()),
     }
+}
+
+fn count_bytes(contents: &String) -> usize {
+    contents.len()
+}
+
+fn count_lines(contents: &String) -> usize {
+    let x: Vec<&str> = contents.lines().collect();
+    x.len()
+}
+
+fn count_characters(contents: &String) -> usize {
+    contents.chars().count()
+}
+
+fn count_words(contents: &String) -> usize {
+    contents.split_whitespace().count()
+}
+
+fn parse_options(options: &String) -> Vec<CcOptions> {
+    let options: Vec<&str> = options.split("").collect();
+    let mut cc_options: Vec<CcOptions> = vec![];
+    for i in options.iter() {
+        match i.to_lowercase().as_str() {
+            "c" => cc_options.push(CcOptions::Bytes),
+            "w" => cc_options.push(CcOptions::Words),
+            "l" => cc_options.push(CcOptions::Lines),
+            "m" => cc_options.push(CcOptions::Characters),
+            _ => {}
+        }
+    }
+    cc_options
 }
 
 fn handle_file_error(error: ErrorKind) -> ExitCode {
