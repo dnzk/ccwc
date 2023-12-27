2e5f6e6c8af51c40a31723b792e5ed02fcfe4bed
Danish Kalesaran <danishkalesaran@gmail.com>
2023-12-27 14:28:47 +0800

    feat: report bytes count from supplied file name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::ErrorKind;
+use std::io::{stderr, stdout, Write};
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        return output("Insufficient arguments", 1);
+    }
+    let file_path = &args[2];
+    match File::open(&file_path) {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).unwrap();
+            return output(
+                format!("{0} has {1} bytes", &file_path, String::len(&contents)).as_str(),
+                0,
+            );
+        }
+        Err(error) => handle_file_error(error.kind()),
+    }
+}
+
+fn handle_file_error(error: ErrorKind) -> ExitCode {
+    let msg = match error {
+        ErrorKind::NotFound => "File not found",
+        ErrorKind::PermissionDenied => "Insufficient access to file",
+        _ => "Fatal error",
+    };
+    return output(msg, 1);
+}
+
+fn output(msg: &str, status: u8) -> ExitCode {
+    if status == 0 {
+        write_to_stdout(msg);
+    } else {
+        write_to_stderr(msg);
+    }
+    return ExitCode::from(status);
+}
+
+fn write_to_stdout(msg: &str) {
+    let mut lock = stdout().lock();
+    writeln!(lock, "{}", msg).expect("Fatal error");
+}
+
+fn write_to_stderr(msg: &str) {
+    let mut lock = stderr().lock();
+    writeln!(lock, "{}", msg).expect("Fatal error");
 }
