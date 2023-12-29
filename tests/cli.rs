99cbb8050838294f84809dcf697d7fe2f349f118
Danish Kalesaran <danishkalesaran@gmail.com>
2023-12-29 10:40:12 +0800

    test: add cli test

---
+++ tests/cli.rs
@@ -0,0 +1,45 @@
+use assert_cmd::Command;
+
+#[test]
+fn shows_lines_count_with_l() {
+    let mut cmd = Command::cargo_bin("ccwc").unwrap();
+    cmd.arg("-l")
+        .arg("./test_files/one.txt")
+        .assert()
+        .stdout("6 lines\n");
+}
+
+#[test]
+fn shows_bytes_count_with_c() {
+    let mut cmd = Command::cargo_bin("ccwc").unwrap();
+    cmd.arg("-c")
+        .arg("./test_files/one.txt")
+        .assert()
+        .stdout("158 bytes\n");
+}
+
+#[test]
+fn shows_characters_count_with_m() {
+    let mut cmd = Command::cargo_bin("ccwc").unwrap();
+    cmd.arg("-m")
+        .arg("./test_files/one.txt")
+        .assert()
+        .stdout("158 characters\n");
+}
+
+#[test]
+fn shows_words_count_with_w() {
+    let mut cmd = Command::cargo_bin("ccwc").unwrap();
+    cmd.arg("-w")
+        .arg("./test_files/one.txt")
+        .assert()
+        .stdout("33 words\n");
+}
+
+#[test]
+fn shows_clw_by_default() {
+    let mut cmd = Command::cargo_bin("ccwc").unwrap();
+    cmd.arg("./test_files/one.txt")
+        .assert()
+        .stdout("158 bytes 6 lines 33 words\n");
+}
